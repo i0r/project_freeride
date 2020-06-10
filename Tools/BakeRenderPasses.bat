458827206aa94621038e89b6aa80d07866ea54ef
i0r <baptiste.prevost@protonmail.com>
2020-06-10 18:04:10 +0200

    [Build] Added prebuilt/baked stuff for faster project setup

---
+++ Tools/BakeRenderPasses.bat
@@ -0,0 +1,1 @@
+..\\build\\bin\\DuskBaker.exe ..\\Assets\\RenderPasses\\ ..\\build\\bin\\data\\shaders\\ ..\\DuskRenderer\\Graphics\\RenderModules\\Generated\\ ..\\DuskEd\\Graphics\\RenderModules\\Reflected\\ 
