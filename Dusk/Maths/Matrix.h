8a041b138f76463354fbef0db98bb3f11e6d7627
i0r <baptiste.prevost@protonmail.com>
2020-06-29 14:45:37 +0200

    [Maths] Added missing vector transform operator

--- Dusk/Maths/Matrix.h
+++ Dusk/Maths/Matrix.h
@@ -280,7 +280,7 @@ struct Matrix<Precision, 3, 3>
 
     }
 
-    constexpr Precision* toArray()
+    constexpr Precision* toArl()
     {
         return &_00;
     }
@@ -336,6 +336,17 @@ static constexpr Matrix<Precision, 4, 4> operator * ( const Matrix<Precision, 4,
         r[3][0] * l[0][1] + r[3][1] * l[1][1] + r[3][2] * l[2][1] + r[3][3] * l[3][1],
         r[3][0] * l[0][2] + r[3][1] * l[1][2] + r[3][2] * l[2][2] + r[3][3] * l[3][2],
         r[3][0] * l[0][3] + r[3][1] * l[1][3] + r[3][2] * l[2][3] + r[3][3] * l[3][3] );
+}
+
+template<typename Precision>
+static constexpr Vector<Precision, 4> operator * ( const Vector<Precision, 4>& l, const Matrix<Precision, 4, 4>& r )
+{
+    return Vector<Precision, 4>(
+        l.x * r[0][0] + l.y * r[1][0] + l.z * r[2][0] + l.w * r[3][0],
+        l.x * r[0][1] + l.y * r[1][1] + l.z * r[2][1] + l.w * r[3][1],
+        l.x * r[0][2] + l.y * r[1][2] + l.z * r[2][2] + l.w * r[3][2],
+        l.x * r[0][3] + l.y * r[1][3] + l.z * r[2][3] + l.w * r[3][3]
+    );
 }
 
 namespace dk
