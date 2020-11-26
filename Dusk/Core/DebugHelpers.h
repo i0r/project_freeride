c0e88d4cf395b1d2ec8e34e0224e006eb1001a95
i0r <baptiste.prevost@protonmail.com>
2020-11-26 23:43:13 +0100

    [Core] Added dummy declaration for breakpoint trigger macro + comments stuff

--- Dusk/Core/DebugHelpers.h
+++ Dusk/Core/DebugHelpers.h
@@ -12,6 +12,8 @@
 // Return true if a debugger is currently attached to this instance; false otherwise.
 bool IsDebuggerAttached();
 
+// Raise a fatal error whenever the condition provided is false (see FatalError above). This should be kept as macro to allow
+// smooth function call stripping.
 #define DUSK_RAISE_FATAL_ERROR( condition, format, ... ) ( condition ) ? (void)0 : FatalError( format "\n", ##__VA_ARGS__ )
 
 #if DUSK_DEVBUILD
@@ -19,14 +21,20 @@ bool IsDebuggerAttached();
 static bool g_SkipAssertions = false;
 
 // If true, trigger a debugger break whenever an assertion fails. Lead to an early app termination if no debugger is attached.
-static bool g_BreakOnAssertionFailure = false;
+static bool g_BreakOnAssertionFailure = true;
 
 #if DUSK_MSVC
 #define DUSK_TRIGGER_BREAKPOINT __debugbreak();
 #elif DUSK_GCC
 #include <signal.h>
 #define DUSK_TRIGGER_BREAKPOINT raise(SIGTRAP);
+#else 
+#define DUSK_TRIGGER_BREAKPOINT
 #endif
+
+// Evaluate a given assertion. If the assertion fails, an error message will be logged and a software breakpoint might
+// be raised (see g_BreakOnAssertionFailure). Assertions can be disabled at compile time/runtime by toggling g_SkipAssertions
+// (see above).
 #define DUSK_DEV_ASSERT( condition, format, ... ) if ( !g_SkipAssertions ) DUSK_ASSERT( condition, format, ##__VA_ARGS__ )
 
 void DumpStackBacktrace();
